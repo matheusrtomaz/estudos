fb1561e7aab682aa7bba1d225935b3def8bfcfa3
Matheus Rodrigues Tomaz <157989074+matheusrtomaz@users.noreply.github.com>
2024-05-21 20:25:32 -0300

    aula 10 nassau

---
+++ JavaScript/UniNassau/atividades/aula10/app.js
@@ -0,0 +1,12 @@
+var n1, n2, media, n3;
+
+function calcularMedia() {
+    n1 = 12;
+    n2 = 10;
+    var resultado = (n1 + n2) / 2;
+    n3 = resultado;
+    return resultado;
+}
+
+calcularMedia();
+console.log(n3);
